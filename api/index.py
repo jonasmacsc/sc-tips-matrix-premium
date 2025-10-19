e87e5d70f381930e8bed8b03c80b93dbdc37ae89
jonasmacsc <vemevesc@gmail.com>
2025-10-19 16:40:52 -0300

    Delete api directory

--- api/index.py
+++
@@ -1,6 +0,0 @@
-import sys, os
-sys.path.append(os.path.dirname(os.path.abspath(__file__)) + '/../')
-from app import app as application
-
-def handler(request, response):
-    return application(request, response)
